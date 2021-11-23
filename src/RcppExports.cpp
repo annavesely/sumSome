ce84ecebf8fb5503257826f00f300a332dda63a9
Anna Vesely <anna.vesely92@gmail.com>
2021-11-23 13:13:23 +0100

    Cran-comments

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // permMin
 bool permMin(const NumericVector& X, const int& B, const double& truncTo);
 RcppExport SEXP _sumSome_permMin(SEXP XSEXP, SEXP BSEXP, SEXP truncToSEXP) {
