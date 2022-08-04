b3b069e7663b64bd59008d6e1b844bc8b2ef196f
Anna Vesely <anna.vesely92@gmail.com>
2022-08-04 15:28:29 +0200

    Revert "Errore"
    This reverts commit 96e3766f1079860b2543c536cc86a1a59294eed3.

--- R/psumStats.R
+++ R/psumStats.R
@@ -2,7 +2,7 @@
 #' @description This function determines confidence bounds for the number of true discoveries, the true discovery proportion
 #' and the false discovery proportion within a set of interest.
 #' The bounds are simultaneous over all sets, and remain valid under post-hoc selection.
-#' @usage sumStatsPar(g, S = NULL, alpha = 0.05, cvs)
+#' @usage psumStats(g, S = NULL, alpha = 0.05, cvs)
 #' @param g numeric vector of statistics.
 #' @param S vector of indices for the variables of interest (if not specified, all variables).
 #' @param alpha significance level.
@@ -14,9 +14,9 @@
 #' \item \code{alpha}: significance level
 #' \item \code{TD}: lower (1-\code{alpha})-confidence bound for the number of true discoveries in \code{S}
 #' \item \code{maxTD}: maximum value of \code{TD} that could be found under convergence of the algorithm
-#' \item \code{iterations}: number of iterations of the algorithm (\code{NULL})
+#' \item \code{iterations}: number of iterations of the algorithm
 #' }
-#' @author Xu Chen.
+#' @author Xu Chen, Anna Vesely.
 #' @examples
 #' # generate vector of statistics for 5 variables (Fisher transformation of p-values)
 #' g <- as.vector(simData(prop = 0.6, m = 5, B = 1, alpha = 0.4, seed = 42))
@@ -29,7 +29,7 @@
 #' cvs <- qchisq(p = 0.4, df = 2 * seq(5), lower.tail=FALSE)
 #'  
 #' # create object of class sumObj
-#' res <- sumStatsPar(g, S, alpha = 0.4, cvs = cvs)
+#' res <- psumStats(g, S, alpha = 0.4, cvs = cvs)
 #' res
 #' summary(res)
 #' 
@@ -47,13 +47,13 @@
 #' Tian, J., Chen, X., Katsevich, E., Goeman, J. J., and Ramdas, A. (2021). Large-scale simultaneous inference under dependence. Scandinavian Journal of Statistics, to appear. (Pre-print arXiv:2102.11253)
 #' 
 #' @seealso
-#' True discovery guarantee using p-values (parametric): \code{\link{sumPvalsPar}}
+#' True discovery guarantee using p-values (parametric): \code{\link{psumPvals}}
 #' 
 #' Access a \code{sumObj} object: \code{\link{discoveries}}, \code{\link{tdp}}, \code{\link{fdp}}
 #' @export
 
 
-sumStatsPar <- function(g, S=NULL, alpha=0.05, cvs){
+psumStats <- function(g, S=NULL, alpha=0.05, cvs){
   
   if(!is.vector(g) || !is.numeric(g) || !all(is.finite(g))){stop("g must be a vector of finite numbers")}
   if(length(g)==0){stop("g must be a vector of finite numbers")}
@@ -70,6 +70,6 @@ sumStatsPar <- function(g, S=NULL, alpha=0.05, cvs){
   if(!is.vector(cvs) || !is.numeric(cvs) || !all(is.finite(cvs))){stop("cvs must be a vector of finite numbers")}
   if(length(g) != length(cvs)){stop("Invalid dimensions: g and cvs must have the same length")}
   
-  out <- sumTestPar(g, S, alpha, cvs)
+  out <- psumTest(g, S, alpha, cvs)
   return(out)
 }
