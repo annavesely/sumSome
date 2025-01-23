b3a65c86a5350edd025018e1924de78d73b36627
Anna Vesely <53512166+annavesely@users.noreply.github.com>
2025-01-23 12:39:31 +0100

    Sistemata pimaAnalysis

--- R/pimaAnalysis.R
+++ R/pimaAnalysis.R
@@ -1,36 +1,69 @@
 #' True discovery guarantee in multiverse analysis
-#' @description This function uses permutation score statistics/p-values to determine a true discovery guarantee for multiverse analysis,
+#' @description This function uses permutation statistics/p-values to determine a true discovery guarantee for multiverse analysis,
 #' when studying one or more parameters of interest within a multiverse of models.
 #' It computes confidence bounds for the number of true discoveries and the true discovery proportion overall or within different groups.
 #' The bounds are simultaneous over all sets, and remain valid under post-hoc selection.
-#' @usage pimaAnalysis(pima, by = NULL, type = "sum", r = 0, alpha = 0.05, ...) 
-#' @param pima an object of class \code{pima}, as obtained from the function \code{\link[pima]{pima}}.
+#' @usage pimaAnalysis(obj, by = NULL, type = "sum", r = 0, alpha = 0.05, ...) 
+#' @param obj an object of class \code{jointest}, as obtained from the functions \code{pima} (package \strong{pima}) or \code{join_flipscores} (\strong{jointest}).
 #' @param by name of grouping element among \code{Coeff} and \code{Model}. If not specified, all coefficients of interest
 #' in all models are considered together.
-#' @param type combining function: \code{sum} uses the sum of scores as in \code{\link[sumSome]{sumStats}},
-#' while different p-value combinations are defined as in \code{\link[sumSome]{sumPvals}}.
+#' @param type combining function: \code{sum} uses the sum of test statistics as in \code{sumStats},
+#' while different p-value combinations are defined as in \code{sumPvals} (\code{edgington}, \code{fisher}, \code{pearson}, \code{liptak},
+#' \code{cauchy}, \code{harmonic}, \code{vovk.wang}).
 #' @param r parameter for Vovk and Wang's p-value combination.
 #' @param alpha significance level.
 #' @param ... further parameters of \code{sumStats} or \code{sumPvals} (truncation parameters and maximum number of iterations of the algorithm).
 #' @details In the default \code{by = NULL}, the procedure computes lower confidence bounds for the
 #' number/proportion of significant effects (non-null coefficients) among all.
 #' Other inputs of the argument \code{by} return analogous bounds, defined by coefficient (\code{"Coeff"}) or by model (\code{"Model"}).
-#' @details If truncation parameters are not specified, scores/p-values are not truncated.
-#' @return Returns a data frame containing a summary for each subset:
+#' While the bounds are simultaneous over all possible groupings,
+#' the combining function \code{type} should be fixed in advance.
+#' @details If truncation parameters are not specified among the further parameters,
+#' statistics/p-values are not truncated.
+#' @details More generically, \code{obj} can be any list containing:
+#' \itemize{
+#' \item \code{Tspace}: data frame of statistics, where columns correspond to variables,
+#' and rows to data transformations (e.g. permutations). The first transformation is the identity.
+#' \item \code{summary_table}: data frame of summary information where rows correspond to variables.
+#' }
+#' @return \code{pimaAnalysis} returns a data frame containing a summary for each subset:
 #' \itemize{
 #' \item \code{size}: number of considered coefficients
-#' \item \code{TD}: lower (1-\code{alpha})-confidence bound for the number of true discoveries
-#' \item \code{TDP}: lower (1-\code{alpha})-confidence bound for the true discovery proportion
+#' \item \code{TD}: lower (1-\code{alpha})-confidence bound for the number of significant effects
+#' \item \code{TDP}: lower (1-\code{alpha})-confidence bound for the proportion of significant effects
 #' }
 #' @references
 #' Girardi, Vesely, Lakens, Altoè, Pastore, Calcagnì, Finos (2024). Post-selection Inference in Multiverse Analysis (PIMA): An Inferential Framework Based on the Sign Flipping Score Test. Psychometrika, doi: 10.1007/s11336-024-09973-6.
 #' 
 #' Vesely, Finos, Goeman (2023). Permutation-based true discovery guarantee by sum tests. Journal of the Royal Statistical Society, Series B (Statistical Methodology), doi: 10.1093/jrsssb/qkad019
 #' @examples
-#' # COPIARE PARTE DELL'ESEMPIO DELLA FUNZIONE pima()
+#' # generate matrix of t-scores for 6 variables (e.g, 2 coefficients X and Z within 3 models)
+#' G <- simData(prop = 0.6, m = 6, B = 50, alpha = 0.4, p = FALSE, seed = 42)
+#' colnames(G) <- rep(c("X","Z"),3)
+#'  
+#' # summary table
+#' summary_table <- data.frame(
+#'   Model = rep(c("mod1","mod2","mod3"), each=2),
+#'   Coeff = colnames(G)
+#' )
+#' 
+#' # list containing the test statistics and the summary
+#' obj <- list(Tspace = as.data.frame(G), summary_table = summary_table)
+#' 
+#' # significant effects overall (sum of test statistics)
+#' pimaAnalysis(obj, alpha = 0.4)
+#' 
+#' # significant effects by coefficient (sum of test statistics)
+#' pimaAnalysis(obj, by = "Model", alpha = 0.4)
+#' 
+#' # significant effects by model (Fisher's combination of p-values)
+#' pimaAnalysis(obj, by = "Coeff", type = "fisher", alpha = 0.4)
+#' 
+#' @seealso
+#' True discovery guarantees: \code{\link{sumStats}}, \code{\link{sumPvals}}
 #' @export
 
-pimaAnalysis <- function(pima, by = NULL, type = "sum", r = 0, alpha = 0.05, ...){
+pimaAnalysis <- function(obj, by = NULL, type = "sum", r = 0, alpha = 0.05, ...){
   
   type <- match.arg(tolower(type), c("sum", "fisher", "pearson", "liptak",  "edgington", "cauchy", "harmonic", "vovk.wang"))
   
@@ -47,8 +80,7 @@ pimaAnalysis <- function(pima, by = NULL, type = "sum", r = 0, alpha = 0.05, ...
     names(S) = uniq_nm
   }
   
-  G <- abs(as.matrix(res$Tspace))
-  alpha <- 1 - conf_level
+  G <- abs(as.matrix(obj$Tspace))
   
   if(type == "sum"){
     
@@ -61,7 +93,7 @@ pimaAnalysis <- function(pima, by = NULL, type = "sum", r = 0, alpha = 0.05, ...
     }
   }else{
     
-    P <- flip::t2p(G, obs.only = FALSE)
+    G <- flip::t2p(G, obs.only = FALSE)
     
     get_disc <- function(x){
       tmp <- do.call(sumSome::sumPvals, c(
@@ -78,7 +110,7 @@ pimaAnalysis <- function(pima, by = NULL, type = "sum", r = 0, alpha = 0.05, ...
   )
   
   out$TDP <- round(out$TD/out$size, 3)
-  names(out) <- names(S)
+  rownames(out) <- names(S)
   
   return(out)
 }
